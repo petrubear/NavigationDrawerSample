e9091e6ddf51811a4e48e4621dd3c3fb4fb0053e
Edison Martinez <edison.martinez@me.com>
2014-02-12 19:09:07 -0500

    Pods update

---
+++ NavigationDrawerSample/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  NavigationDrawerSample
+//
+//  Created by Edison Martinez on 2/12/14.
+//  Copyright (c) 2014 Cronix cia. ltda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UIViewController
+
+@end
